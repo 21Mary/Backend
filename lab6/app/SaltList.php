fbafed0570f502c3c4cd969d7513912c71473945
21Mary <Marichka.Mymokhod@gmail.com>
2025-12-18 01:39:06 +0200

    Lab6

---
+++ lab6/app/SaltList.php
@@ -0,0 +1,130 @@
+<?php
+require_once('BaseList.php');
+require_once('Salt.php');
+require_once('DBConnect.php');
+class SaltList extends BaseList{
+    public function add($params){
+        $elem=new Salt($params['id'],$params['name'],$params['formula'],$params['marking'],$params['type_id'],$params['typename'],$params['purpose_id'],$params['purposename']);
+        array_push($this->list, $elem);
+    }
+    public function update($params){
+        for ($i=0;$i<count($this->list);$i++){
+            if($this->list[$i]->getId()==$params['id']){
+                $this->list[$i]->update($params['name'],$params['formula'],$params['marking'],$params['type_id'],$params['purpose_id']);
+                break;
+            }
+        }
+    }
+    public function getAsJSON(){
+        $content='{
+    "salts": [';
+        for ($i=0;$i<count($this->list);$i++){
+            $content.=$this->list[$i]->getAsJSON().",";
+        }
+        $content = substr($content, 0, -1);
+        $content.='    ]
+        }';
+        return $content;
+    }
+    public function getAsXML(){
+        $content='<salts>
+        ';
+        for ($i=0;$i<count($this->list);$i++){
+            $content.=$this->list[$i]->getAsXML();
+        }
+        $content.='</salts>';
+        return $content;
+    }
+    public function readFromCSV($filePath){
+        $fp = fopen($filePath, 'r');
+        if ($fp === false) {
+            die('Error: Cannot open the CSV file.');
+        }
+        while (($row = fgetcsv($fp,10000,",","`","\\")) !== false) {
+            $this->add(['name'=>$row[0],'formula'=>$row[1],'marking'=>$row[2],'type_id'=>$row[3],'purpose_id'=>$row[4]]);
+        }
+        fclose($fp);
+    }
+    public function getAllFromDatabase(){
+        global $conn;
+        $sql = "SELECT salts.*, types.name AS typename, purpose.name AS purposename
+        FROM salts
+        INNER JOIN types ON types.id=salts.type_id
+        INNER JOIN purpose ON purpose.id = salts.purpose_id ORDER BY salts.id ASC";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $this->add($row);
+        }
+        }
+    }
+    public function getAllFromDatabaseById($id){
+        global $conn;
+        $stmt = $conn->prepare("SELECT salts.*, types.name AS typename, purpose.name AS purposename
+        FROM salts
+        INNER JOIN types ON types.id = salts.type_id
+        INNER JOIN purpose ON purpose.id = salts.purpose_id WHERE salts.id = ?");
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            return $row;
+        }
+        } else{
+            return null;
+        }
+    }
+    public function addSaltProperty($salt_id,$property_id,$value){
+        global $conn;
+        $stmt = $conn->prepare("INSERT INTO salt_properties VALUES (DEFAULT, ?,?,?)");
+        $stmt->bind_param("sss", $salt_id,$property_id, $value);
+        $stmt->execute();
+        return $conn->insert_id;
+    }
+    public function updateSaltProperty($salt_id,$property_id,$value){
+        global $conn;
+        $stmt = $conn->prepare("SELECT * from `salt_properties` WHERE `salt_id`=? and `property_id`=?;");
+        $stmt->bind_param("ss", $salt_id,$property_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $stmt = $conn->prepare("UPDATE `salt_properties` SET `value`=? WHERE `salt_id`=? and `property_id`=?;");
+            $stmt->bind_param("sss", $value,$salt_id,$property_id);
+            $stmt->execute();
+        } else{
+            $stmt = $conn->prepare("INSERT INTO salt_properties VALUES (DEFAULT, ?,?,?)");
+            $stmt->bind_param("sss", $salt_id,$property_id, $value);
+            $stmt->execute();
+        }
+    }
+    public function insertIntoDatabase($params){
+        global $conn;
+        $stmt = $conn->prepare("INSERT INTO salts  VALUES (DEFAULT, ?,?,?,?,?)");
+        $stmt->bind_param("sssss", $params['name'],$params['formula'],$params['marking'],$params['type_id'],$params['purpose_id']);
+        $stmt->execute();
+        return $conn->insert_id;
+    }
+    public function updateDatabaseById($params){
+        global $conn;
+        $stmt = $conn->prepare("UPDATE `salts` SET `name`=?, `formula`=?,`marking`=?, `type_id`=?, `purpose_id`=? WHERE `id`=?");
+        $stmt->bind_param("ssssss", $params['name'],$params['formula'],$params['marking'],$params['type_id'],$params['purpose_id'],$params['id']);
+        $stmt->execute();
+    }
+    public function deleteFromDatabaseById($id){
+        global $conn;
+        $stmt = $conn->prepare("DELETE FROM salts WHERE id=?");
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+    }
+    public function getSaltPropertiesById($id){
+        for ($i=0;$i<count($this->list);$i++){
+            if($this->list[$i]->getId()==$id){
+                return $this->list[$i]->getSaltProperties();
+            }
+        }
+    }
+}
---
+++ lab6/ssr/salts.php
@@ -0,0 +1,131 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+if(!$_SESSION['user']){
+    header('Location: login.php');
+}
+require_once('../app/SaltList.php');
+require_once('../app/TypeList.php');
+require_once('../app/PurposeList.php');
+require_once('../app/PropertyList.php');
+$typList=new TypeList();
+$typList->getAllFromDatabase();
+$purList=new PurposeList();
+$purList->getAllFromDatabase();
+$propList=new PropertyList();
+$propList->getAllFromDatabase();
+$propArray=$propList->getAsAssocArray();
+$a = new SaltList();
+$item=null;
+$itemProps=[];
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    if($_POST['id']==""){
+        $salt_id=$a->insertIntoDatabase(['name'=>$_POST['name'], 
+        'formula'=>$_POST['formula'],
+        'type_id'=>$_POST['typeid'],
+        'purpose_id'=>$_POST['purposeid'], 
+        'marking'=>$_POST['marking'],
+]);
+    for ($i=0;$i<count($propArray);$i++){
+        if(isset($_POST['prop-'.$propArray[$i]['id']])){
+            $a->addSaltProperty($salt_id,$propArray[$i]['id'],$_POST['prop-'.$propArray[$i]['id']]);
+        }
+    }
+    } else{
+        $propArray=$propList->getAsAssocArray();
+        $a->updateDatabaseById(['id'=>$_POST['id'],
+        'name'=>$_POST['name'], 
+        'formula'=>$_POST['formula'],
+        'type_id'=>$_POST['typeid'], 
+        'purpose_id'=>$_POST['purposeid'], 
+        'marking'=>$_POST['marking']]);
+        for ($i=0;$i<count($propArray);$i++){
+        if(isset($_POST['prop-'.$propArray[$i]['id']])&&$_POST['prop-'.$propArray[$i]['id']]!='' ){
+            
+            $a->updateSaltProperty($_POST['id'],$propArray[$i]['id'],$_POST['prop-'.$propArray[$i]['id']]);
+        }
+        }    
+        header('Location: salts.php');
+    }
+    
+    $a->getAllFromDatabase();
+} else{
+    $a->getAllFromDatabase();
+    if(isset($_GET['action'])&&$_GET['action']=='delete'){
+        $a->deleteFromDatabaseById($_GET['id']);
+        header('Location: salts.php');
+    } else if(isset($_GET['action'])&&$_GET['action']=='update'){
+        $item=$a->getById($_GET['id']);
+        $itemProps=$a->getSaltPropertiesById($_GET['id']);
+    }
+    
+}
+?>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Типи</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
+    </head>
+    <body>
+        <div class="container">
+            <ul class="nav">
+                <li><a class="btn btn-outline nav-btn" href="./types.php">Типи</a></li>
+                <li><a class="btn btn-outline nav-btn" href="./purpose.php">Сфера призначення</a></li>
+                <li><a class="btn btn-outline nav-btn" href="./properties.php">Характеристики</a></li>
+                <li><a class="btn btn-outline nav-btn" href="./salts.php">Солі</a></li>
+                <li><a class="btn btn-outline nav-btn" href="./logout.php">Вийти</a></li>
+            </ul>
+            <h1>Типи</h1>
+            <div class="row">
+                <div class="col-md-8">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Назва</th>
+                                <th>Позначення</th>
+                                <th>Тип</th>
+                                <th>Сфера призначення</th>
+                                <th>Маркування</th>
+                                <th>Характеристики</th>
+                                <th>Дії</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php echo $a->getAsTableBody();?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-4">
+                    <form method="POST">
+                        <p>
+                            <input type="text" name="name" value="<?php echo $item?$item['name']:'';?>" class="form-control" placeholder="Назва" required/>
+                        </p>
+                        <p>
+                            <input type="text" name="formula" value="<?php echo $item?$item['formula']:'';?>" class="form-control" placeholder="Позначення" required/>
+                        </p>
+                        <p>
+                            <select name="typeid" class="form-select" placeholder="Тип" required><?php echo $typList->getAsSelectOptions($item?$item['type_id']:'');?></select>
+                        </p>
+                        <p>
+                            <select name="purposeid" class="form-select" placeholder="Сфера призначення" required><?php echo $purList->getAsSelectOptions($item?$item['purpose_id']:'');?></select>
+                        </p>                        
+                        <p>
+                            <input type="text" name="marking" value="<?php echo $item?$item['marking']:'';?>" class="form-control" placeholder="Маркування" required/>
+                            
+                        </p>
+                        <?php echo $propList->getAsInputGroup($itemProps); ?>
+                        <p>
+                            <input type="hidden" name="id" value="<?php echo $item?$item['id']:'';?>"/>
+                            <button class="btn btn-success" type="submit">Зберегти</button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</html>
